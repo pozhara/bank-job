d5d6ebcdea2b548151e35e465c993d1294e57546
pozhara <poliazhara04@gmail.com>
2023-01-16 14:18:59 +0000

    Add function to greet user and ask what they want to do

--- run.py
+++ run.py
@@ -13,6 +13,20 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("office-work")
 
 employees = SHEET.worksheet("Employees")
-data = employees.get_all_values()
 
-print(data)
+def description():
+    print("Hello, we are a recently opened bank, looking for qualified people to start their career with us.")
+
+
+def get_user_input():
+    while True:
+        print("What would you like to do?\n1. Add youself as an employee\n2. Request a time off\n3. See everyone's birthdays")
+        print("Please enter a number below to choose and hit ENTER.")
+        choice = input()
+    return choice
+
+def main():
+    description()
+    get_user_input()
+
+main()
