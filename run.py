99326f91c3882c804c5602486469154071196a03
pozhara <poliazhara04@gmail.com>
2023-01-30 19:04:37 +0000

    Add comments

--- run.py
+++ run.py
@@ -1,38 +1,48 @@
-import gspread
+import gspread # to use google sheets
 from google.oauth2.service_account import Credentials
-import datetime
-import math
-import sys
-import time
-from os import system
+import datetime # to check age
+import math # to check decimal
+import sys # to exit the program
+import time # to add pauses
+from os import system # to clear terminal
 
+# Defines the scope
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+# Adds credentials to the account and authorises the client sheet
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("office-work")
 
+# A greeting, instruction on what to do next
 print("Hello, we are a recently opened bank, thank you for starting your career with us. Your next step is to add yourself as an employee to our system.\n")
 
 def clear():
+    # Clears the terminal
     system('clear')
 
 
 def wait():
+    # Adds pause before going on
     time.sleep(2.5)
 
 def update_worksheet(data, worksheet):
+    # Updates worksheet
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
 
 while True:
     try:
-        first_name = input("Please enter your first name: ")
+        """ 
+        Asks for first name, checks for length, the input not being a number or null.
+        Raises ValueError if input isn't valid.
+        """
+        first_name = input("Please enter your first name(maximum 20 characters): ")
         print(first_name)
         if len(first_name) < 1 or len(first_name) > 20 or first_name.isnumeric():
             raise ValueError
@@ -42,7 +52,11 @@ while True:
 
 while True:
     try:
-        last_name = input("Please enter your last name: ")
+        """ 
+        Asks for first name, checks for length, the input not being a number or null.
+        Raises ValueError if input isn't valid.
+        """
+        last_name = input("Please enter your last name(maximum 20 characters): ")
         print(last_name)
         if len(last_name) < 1 or len(last_name) > 20 or last_name.isnumeric():
             raise ValueError
@@ -51,7 +65,11 @@ while True:
         print("Please enter valid data.\n")
 
 while True:
-    try: 
+    try:
+        """
+        Asks for birth day, checks for input being a number between 1 and 31.
+        If it's lower, higher, null or a string, raises ValueError.
+        """
         age_day = int(input("Please enter the day you were born: "))
         print(age_day)
         if age_day > 31 or age_day < 1:
@@ -61,7 +79,11 @@ while True:
         print('Value must be positive and cannot be greater than 31.\n')
 
 while True:
-    try: 
+    try:
+        """
+        Asks for birth month, checks for input being a number between 1 and 12.
+        If input is lower, higher, null or a string, raises ValueError.
+        """
         age_month = int(input("Please enter the month you were born: "))
         print(age_month)
         if age_month > 12 or age_month < 1:
@@ -71,7 +93,13 @@ while True:
         print('Value must be positive and must be between 1 and 12.\n')
 
 while True:
-    try: 
+    try:
+        """
+        Asks for birth year, calculates age of an employee.
+        Checks for it to be between 18 and 80.
+        If it's not, raises ValueError.
+        If it is, updates birthday worksheet.
+        """
         age_year = int(input("Please enter the year you were born: "))
         print(age_year)
         date_of_birth = datetime.datetime(age_year, age_month, age_day)
@@ -92,7 +120,12 @@ while True:
 
 while True:
     try:
-        employee_role = input("Please enter your role: ")
+        """
+        Asks for a role. Checks for length and input being a number or null.
+        If data is valid, it is added to employees worksheet.
+        If it isn't, raises a ValueError.
+        """
+        employee_role = input("Please enter your role(maximum 20 characters): ")
         print(employee_role)
         if len(employee_role) < 1 or len(employee_role) > 20 or employee_role.isnumeric():
             raise ValueError
@@ -107,6 +140,11 @@ while True:
         print("Please enter valid data.\n")
 
 def give_options():
+    """
+    Asks user what they want to do, checks user input to be a number between 1 and 4.
+    If it is, clears terminal, waits a bit and does what the user chose.
+    If it's not, raises ValueError.
+    """
     print("What would you like to do?\n1. Request a day off.\n2. See your collegues' birthdays.\n3. See your collegues' names and roles.\n4. Exit.")
     while True:
         try:
@@ -126,33 +164,55 @@ def give_options():
     wait()
 
 def request_a_day_off(first_name, last_name):
+    """
+    Asks for starting and ending date and a reason for a day off. 
+    If the data is valid, request a day off worksheet is updated, 
+    waits a bit and asks what user wants to do next.
+    """
     print("You are currently requesting a day off. We will need you to provide starting and ending date, and a reason.\n")
     while True:
         print(f"Your name is {first_name} {last_name}.")
         try:
+            """
+            Asks for starting date.
+            If it's lower than 01.01, higher than 31.12, is integer,
+            numbers after comma are higher than 12 and if there are 
+            more than 2 numbers after comma, raises a ValueError.
+            """
             starting_date = float(input("Please enter a starting date (For example: 12.02): "))
             print(starting_date)
             whole = math.floor(starting_date)
             frac = starting_date - whole
             needed_decimal = '0.23'
-            if starting_date > 31.12 or starting_date < 01.01 or starting_date.is_integer() or frac > 0.31 or len(needed_decimal) > len(str(starting_date)):
+            if starting_date > 31.12 or starting_date < 01.01 or starting_date.is_integer() or frac > 0.12 or len(needed_decimal) > len(str(starting_date)):
                 raise ValueError
             break
         except ValueError:
             print("Invalid data, please provide it like this: 12.02\n")
     while True:
         try:
+            """
+            Asks for ending date.
+            If it's lower than 01.01, higher than 31.12, is integer,
+            numbers after comma are higher than 12 or there are 
+            more than 2 numbers after comma, raises a ValueError.
+            """
             ending_date = float(input("Please enter an ending date (For example: 12.02): "))
             print(ending_date)
             whole_two = math.floor(ending_date)
             frac_two = ending_date - whole_two
-            if ending_date > 31.12 or ending_date < 01.01 or ending_date.is_integer() or frac_two > 0.31 or len(needed_decimal) > len(str(ending_date)):
+            if ending_date > 31.12 or ending_date < 01.01 or ending_date.is_integer() or frac_two > 0.12 or len(needed_decimal) > len(str(ending_date)):
                 raise ValueError
             break
         except ValueError:
             print("Invalid data, please provide it like this: 12.02\n")
     while True:
         try:
+            """
+            Asks for a reason.
+            If it's length is higher than 25 or null, or if it's a number, raises ValueError.
+            Otherwise, updates day off requests worksheet and thanks the user.
+            """
             user_reason = input("Please provide a reason (maximum 25 characters): ")
             print(user_reason)
             if len(user_reason) > 25 or len(user_reason) < 1 or user_reason.isnumeric():
@@ -170,6 +230,10 @@ def request_a_day_off(first_name, last_name):
     give_options()
 
 def see_birthdays():
+    """
+    Prints out employees' names and birthdays. 
+    Then waits and asks the user what they want to do.
+    """
     birthdays = SHEET.worksheet("Birthday").get_all_values()
     for row in birthdays:
         first_name_birthday = row[0]
@@ -183,6 +247,10 @@ def see_birthdays():
 
 
 def see_roles():
+    """
+    Prints out employees and their roles.
+    Then waits and asks the user what they want to do.
+    """
     employees = SHEET.worksheet("Employees").get_all_values()
     for row in employees:
         employee_fname = row[0]
@@ -194,6 +262,10 @@ def see_roles():
     give_options()
 
 def main(first_name, last_name):
+    """
+    Main function which calls other functions
+    based on user input.
+    """
     give_options()
     if user_input == 1:
         request_a_day_off(first_name, last_name)
@@ -205,4 +277,5 @@ def main(first_name, last_name):
         clear()
         sys.exit("You are now exiting the program. Thank you!")
 
+# Calling the main function
 main(first_name, last_name)
